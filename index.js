a221c0e763aecaea916bd7b2fd83a4a8b9753c2b
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-01-26 12:24:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,36 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 
 // Add your functions and code here
-function destructivelyAppendKitten (kitten) {
+function destructivelyAppendKitten (name) {
   kittens.push(kitten);
 }
+
+function destructivelyPrependKitten (name) {
+  kittens.unshift(kitten),
+}
+
+function destructivelyRemoveLastKitten () {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten () {
+  kittens.shift();
+}
+
+function appendKitten (name) {
+  return [...kittens, kitten];
+}
+
+function prependKitten (name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten () {
+  return kittens.slice(0, kittens.length-1);
+}
+
+function removeFirstKitten () {
+  return kittens.slice(1);
+}
+
+
